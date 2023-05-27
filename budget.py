544f3acfd5a9f1f97f2384b92529ec7e6aea2049
Alexander Rivas <rivasoraclea@protonmail.com>
2023-05-27 01:20:44 -0500

    wrong virtual env

---
+++ budget.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+FILEPATH = "buddy_export.csv"
+
+
+def total_spent_by_head_category(budget: pd.Dataframe):
+    budget = budget[budget.HEAD_CATEGORY != "Income"]
+    print(budget.groupby(by=['HEAD_CATEGORY']).sum()['AMOUNT'])
+
+
+def total_spent_by_category(budget: pd.Dataframe):
+    budget = budget[budget.HEAD_CATEGORY != "Income"]
+    print(budget.groupby(by=['CATEGORY']).sum()['AMOUNT'])
+
+
+def main():
+    # I need to add an AMOUNT column to the csv before running this
+    budget_dataframe = pd.read_csv(FILEPATH, sep=';')
+    budget_dataframe.rename(columns={
+                                        'Head categor': 'HEAD_CATEGORY', 
+                                        'Date': 'DATE', 
+                                        'Category': 'CATEGORY', 
+                                        'Note': 'NOTE', 
+                                        'Amount': 'AMOUNT'
+                                    },
+                            inplace=True)
+
+    
+    budget_dataframe = budget_dataframe[~budget_dataframe.NOTE.str.contains("Tio Carlos", na=False)]
+
+
+
+if __name__ == '__main__':
+    main()
