5383e6cc67915bc27f5a4f10969c8ac41155341b
Alexander Rivas <rivasoraclea@protonmail.com>
2023-05-27 13:33:01 -0500

    extract to func

--- budget.py
+++ budget.py
@@ -4,12 +4,12 @@ import pandas as pd
 FILEPATH = "buddy_export.csv"
 
 
-def total_spent_by_head_category(budget: pd.Dataframe):
+def total_spent_by_head_category(budget: pd.DataFrame):
     budget = budget[budget.HEAD_CATEGORY != "Income"]
     print(budget.groupby(by=['HEAD_CATEGORY']).sum()['AMOUNT'])
 
 
-def total_spent_by_category(budget: pd.Dataframe):
+def total_spent_by_category(budget: pd.DataFrame):
     budget = budget[budget.HEAD_CATEGORY != "Income"]
     print(budget.groupby(by=['CATEGORY']).sum()['AMOUNT'])
 
@@ -26,9 +26,10 @@ def main():
                                     },
                             inplace=True)
 
-    
-    budget_dataframe = budget_dataframe[~budget_dataframe.NOTE.str.contains("Tio Carlos", na=False)]
+    print(budget_dataframe[budget_dataframe.HEAD_CATEGORY == "Miscellaneous"])
 
+    budget_dataframe = budget_dataframe[~budget_dataframe.NOTE.str.contains("Tio Carlos", na=False)]
+    total_spent_by_category(budget_dataframe)
 
 
 if __name__ == '__main__':
